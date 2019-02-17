7d9ce6fcc91b25ea915da46732e64d5d1949a31f
Eddie Atkinson <eddiebatkinson@gmail.com>
2019-02-17 17:12:04 -0500

    init commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <TextInput
+          placeholder='Number of comments'
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
